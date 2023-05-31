eeb75ea622c1f34e43492b9e6c8cb633cf0524bc
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 11:36:11 +0900

    [🎨스타일] form 스타일 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import "./App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <form>
+        <label htmlFor="first-name">First Name: </label>
+        <input id="first-name" type="text" />
+        <label htmlFor="last-name">Last Name: </label>
+        <input id="last-name" type="text" />
+        <label htmlFor="email">Email: </label>
+        <input id="email" type="email" />
+        <label htmlFor="age">Age: </label>
+        <input id="age" type="number" />
+        <label htmlFor="password">Password: </label>
+        <input id="password" type="password" />
+        <label htmlFor="confirm-password">Confirm Password: </label>
+        <input id="confirm-password" type="password" />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
