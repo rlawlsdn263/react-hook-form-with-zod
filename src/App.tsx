31f3840540ec7c65093f450da745551d69d60e36
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 11:43:07 +0900

    [✍학습] Zod의 z 객체로 schema 만들기
    - form에 들어가는 각 값의 타입과 길이를 체크하는 schema를 만듦

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import "./App.css";
+import { z } from "zod";
 
 function App() {
+  const schema = z.object({
+    firstName: z.string().min(2).max(30),
+    lastName: z.string().min(2).max(30),
+    email: z.string().email(),
+    age: z.number().min(18).max(70),
+    password: z.string().min(5).max(20),
+    confirmPassword: z.string().min(5).max(20),
+  });
+
   return (
     <div className="App">
       <form>
