e6940b4ce8b647e8f69f229a0fc720e56edf3710
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 11:44:43 +0900

    [✍학습] refine 사용해 비밀번호 일치 확인하기
    - refine을 사용해 만들어둔 객체 중에서 password와 confirmPassword의 값이 일치하는 지를 확인할 수 있음
    - message로 원하는 에러 메세지와 path로 이동하는 경로를 설정할 수 있음

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import "./App.css";
 import { z } from "zod";
 
 function App() {
-  const schema = z.object({
-    firstName: z.string().min(2).max(30),
-    lastName: z.string().min(2).max(30),
-    email: z.string().email(),
-    age: z.number().min(18).max(70),
-    password: z.string().min(5).max(20),
-    confirmPassword: z.string().min(5).max(20),
-  });
+  const schema = z
+    .object({
+      firstName: z.string().min(2).max(30),
+      lastName: z.string().min(2).max(30),
+      email: z.string().email(),
+      age: z.number().min(18).max(70),
+      password: z.string().min(5).max(20),
+      confirmPassword: z.string().min(5).max(20),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Password do not match",
+      path: ["confirmPassword"],
+    });
 
   return (
     <div className="App">
