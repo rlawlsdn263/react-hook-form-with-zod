7f6490afbfd32fcf0d4101e3206ba22a9553b0a7
rlawlsdn263 <rlawlsdn263@naver.com>
2023-05-31 12:16:00 +0900

    [✍학습] errors로 에러 메세지 전달하기
    - React-Hook-Form의 errors 기능을 사용해 각 input의 유효성 검사가 실패하면 에러 메세지를 띄우게 함

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,11 @@ function App() {
       path: ["confirmPassword"],
     });
 
-  const { register, handleSubmit } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
@@ -40,24 +44,37 @@ function App() {
       <form onSubmit={handleSubmit(submitData)}>
         <label htmlFor="first-name">First Name: </label>
         <input id="first-name" type="text" {...register("firstName")} />
+        {errors.firstName && <span>{errors.firstName.message}</span>}
+
         <label htmlFor="last-name">Last Name: </label>
         <input id="last-name" type="text" {...register("lastName")} />
+        {errors.lastName && <span>{errors.lastName.message}</span>}
+
         <label htmlFor="email">Email: </label>
         <input id="email" type="email" {...register("email")} />
+        {errors.email && <span>{errors.email.message}</span>}
+
         <label htmlFor="age">Age: </label>
         <input
           id="age"
           type="number"
           {...register("age", { valueAsNumber: true })}
         />
+        {errors.age && <span>{errors.age.message}</span>}
+
         <label htmlFor="password">Password: </label>
         <input id="password" type="password" {...register("password")} />
+        {errors.password && <span>{errors.password.message}</span>}
+
         <label htmlFor="confirm-password">Confirm Password: </label>
         <input
           id="confirm-password"
           type="password"
           {...register("confirmPassword")}
         />
+        {errors.confirmPassword && (
+          <span>{errors.confirmPassword.message}</span>
+        )}
 
         <input type="submit" />
       </form>
